9b72cfcbf71b9ea81f3e8fdeb0eca0542d253b7e
Albert Ndur-Osei <albertndurosei@gmail.com>
2020-10-17 20:19:22 -0400

    Update sketch_sep17a.ino

--- Arduino/sketch_sep17a.ino
+++ Arduino/sketch_sep17a.ino
@@ -1,3 +1,4 @@
+//https://maker.pro/arduino/tutorial/how-to-make-arduino-and-processing-ide-communicate#:~:text=Processing%20is%20a%20great%20source,works%20for%20a%20micro%2Dcontroller.&text=It%20has%20setup%20functions%20and,Arduino%20IDE%20through%20serial%20communication.
 char val; // Data received from the serial port
 int ledPin = 13; // Set the pin to digital I/O 13
 
